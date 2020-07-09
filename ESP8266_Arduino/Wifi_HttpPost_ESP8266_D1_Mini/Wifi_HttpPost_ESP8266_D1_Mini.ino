2d7831e3cd53595e4576e14acf2733accd59d8f1
jb1nbg <57185929+jb1nbg@users.noreply.github.com>
2020-07-09 19:56:21 +0200

    Issue 3 deepsleep (#7)
    * first measure and in step two activate wifi and push data. only measure one time and use this data for debug output and push data

--- ESP8266_Arduino/Wifi_HttpPost_ESP8266_D1_Mini/Wifi_HttpPost_ESP8266_D1_Mini.ino
+++ ESP8266_Arduino/Wifi_HttpPost_ESP8266_D1_Mini/Wifi_HttpPost_ESP8266_D1_Mini.ino
@@ -1,6 +1,6 @@
 /*
  * Board: LOLIN(WEMOS) D1 R2 & mini 
- * 
+ * Required Libraries: BMx280MI
  * 
  */
 
@@ -21,16 +21,15 @@
 #define WIFI_PASS "[Password]"
 #endif
 
+#define DEEP_SLEEP
+#define MEASURE_PERIOD_SEC    30
+
 //Your Domain name with URL path or IP address with path
 const char* serverName = "http://192.168.178.46:10000/post_temperature_data.php";
 
 // the following variables are unsigned longs because the time, measured in
 // milliseconds, will quickly become a bigger number than can be stored in an int.
 unsigned long lastTime = 0;
-// Timer set to 10 minutes (600000)
-//unsigned long timerDelay = 600000;
-// Set timer to 60 seconds (60000)
-unsigned long timerDelay = 60000;
 
 #ifndef NTP_SERVER
 #define NTP_SERVER    "192.168.178.1"
@@ -39,17 +38,29 @@ unsigned long timerDelay = 60000;
 //create a BMx280I2C object using the I2C interface with I2C Address 0x76
 BMx280I2C bmx280(0x76);
 
+void doWork();
+void setup_wifi();
+void setup_ntp();
+bool setup_bmx280();
+void pushData(char* timestamp, float temp, double pressure, float humidity);
+
 void printLocalTime()
 {
   Serial.println(DateTime.toString());
 }
 
-void setup()
+void printBMX280(float temp, double pressure, float humidity)
 {
-  delay(1000);
-  Serial.begin(115200);
- 
+  Serial.print("Temperature: "); Serial.print(temp); Serial.println(" °C"); 
+  Serial.print("Pressure (64 bit): "); Serial.print(pressure / 100); Serial.println(" mBar");
+  Serial.print("Humidity: "); Serial.print(humidity); Serial.println(" %");
+}
+
+void setup_wifi()
+{
   WiFi.begin( WIFI_SSID, WIFI_PASS );
+  WiFi.setOutputPower(0); // this sets wifi to lowest power
+  //WiFi.setOutputPower(20.5); // this sets wifi to highest power
 
   Serial.println();
   Serial.print("Connecting");
@@ -61,8 +72,11 @@ void setup()
 
   Serial.println("success!");
   Serial.print("IP Address is: ");
-  Serial.println(WiFi.localIP());
+  Serial.println(WiFi.localIP());  
+}
 
+void setup_ntp()
+{
   //init and get the time
   DateTime.setServer( NTP_SERVER );
   DateTime.setTimeZone( -2 );
@@ -70,44 +84,123 @@ void setup()
   if (!DateTime.isTimeValid()) {
     Serial.println("Failed to get time from server.");
   }
-  printLocalTime();
-
-  Wire.begin();
-
-  //begin() checks the Interface, reads the sensor ID (to differentiate between BMP280 and BME280)
-  //and reads compensation parameters.
-  if (!bmx280.begin())
-  {
-    Serial.println("begin() failed. check your BMx280 Interface and I2C Address.");
-    while (1);
-  }
-
-  if (bmx280.isBME280())
-    Serial.println("sensor is a BME280");
-  else
-    Serial.println("sensor is a BMP280");
-
-  //reset sensor to default parameters.
-  bmx280.resetToDefaults();
-
-  //by default sensing is disabled and must be enabled by setting a non-zero
-  //oversampling setting.
-  //set an oversampling setting for pressure and temperature measurements. 
-  bmx280.writeOversamplingPressure(BMx280MI::OSRS_P_x16);
-  bmx280.writeOversamplingTemperature(BMx280MI::OSRS_T_x16);
-
-  //if sensor is a BME280, set an oversampling setting for humidity measurements.
-  if (bmx280.isBME280())
-    bmx280.writeOversamplingHumidity(BMx280MI::OSRS_H_x16);  
+  printLocalTime();  
 }
 
-void loop() 
-{  
-  //Send an HTTP POST request every 10 minutes
-  if ((millis() - lastTime) > timerDelay) 
+bool setup_bmx280()
+{
+  Wire.begin();
+  
+  //begin() checks the Interface, reads the sensor ID (to differentiate between BMP280 and BME280)
+  //and reads compensation parameters.
+  if (bmx280.begin())
   {
+    if (bmx280.isBME280())
+      Serial.println("sensor is a BME280");
+    else
+      Serial.println("sensor is a BMP280");
+  
+    //reset sensor to default parameters.
+    bmx280.resetToDefaults();
+  
+    //by default sensing is disabled and must be enabled by setting a non-zero
+    //oversampling setting.
+    //set an oversampling setting for pressure and temperature measurements. 
+    bmx280.writeOversamplingPressure(BMx280MI::OSRS_P_x16);
+    bmx280.writeOversamplingTemperature(BMx280MI::OSRS_T_x16);
+  
+    //if sensor is a BME280, set an oversampling setting for humidity measurements.
+    if (bmx280.isBME280())
+      bmx280.writeOversamplingHumidity(BMx280MI::OSRS_H_x16);
+
+    return true;
+  }
+  else
+  {
+    Serial.println("bmx280.begin() failed. check your BMx280 Interface and I2C Address."); 
+    return false;   
+  }
+}
+
+void deepSleep()
+{
+  Serial.print("Deep sleep for ");
+  Serial.print(MEASURE_PERIOD_SEC, DEC);
+  Serial.println(" seconds");
+
+  ESP.deepSleep(MEASURE_PERIOD_SEC * 1000 * 1000); // in uSec
+}
+
+void setup()
+{
+  Serial.begin(74880);
+  
+  if (setup_bmx280())
+  {
+#ifdef DEEP_SLEEP
+    doWork();
+    deepSleep();
+#endif
+  }
+  else
+  {
+#ifdef DEEP_SLEEP
+    deepSleep();
+#endif    
+  }
+}
+
+void pushData(char* timestamp, float temp, double pressure, float humidity)
+{
+  HTTPClient http;
+  
+  // Your Domain name with URL path or IP address with path
+  http.begin(serverName);
+
+  // Specify content-type header
+  http.addHeader("Content-Type", "application/x-www-form-urlencoded");
+  // Data to send with HTTP POST
+  String httpRequestData = "api_key=temperature_logger&timestamp=";
+  httpRequestData += timestamp;
+  httpRequestData += "&temperature=" + String(temp);
+  httpRequestData += "&pressure=" + String(pressure / 100);
+  httpRequestData += "&humidity=" + String(humidity);
+  Serial.println(httpRequestData);
+      
+  // Send HTTP POST request
+  int httpResponseCode = http.POST(httpRequestData);
+ 
+  Serial.print("HTTP Response code: ");
+  Serial.println(httpResponseCode);
+    
+  // Free resources
+  http.end();  
+}
+
+void doWork()
+{
+  //start a measurement
+  if (bmx280.measure())
+  {
+    //wait for the measurement to finish
+    do
+    {
+      delay(100);
+    } while (!bmx280.hasValue());
+
+    float temp = bmx280.getTemperature();
+    double pressure = bmx280.getPressure64();
+    float humidity = 0;
+    if (bmx280.isBME280())
+      humidity = bmx280.getHumidity();
+    
+    printBMX280(temp, pressure, humidity);
+          
+    setup_wifi();
+    setup_ntp();
+  
     //Check WiFi connection status
-    if (WiFi.status()== WL_CONNECTED)
+    if (WiFi.status() == WL_CONNECTED)
     {    
       if (!DateTime.isTimeValid()) 
       {
@@ -122,64 +215,32 @@ void loop()
                 p.getMonth(), p.getMonthDay(), p.getHours(), p.getMinutes(),
                 p.getSeconds(), p.getTime(), p.getTimeZone());
         */      
-        char logString[20];
-        sprintf(logString,"%04d-%02d-%02d %02d.%02d.%02d", p.getYear(), p.getMonth(), p.getMonthDay(), p.getHours(), p.getMinutes(), p.getSeconds());
-        Serial.println(logString);
-
-        //start a measurement
-        if (bmx280.measure())
-        {
-          //wait for the measurement to finish
-          do
-          {
-            delay(100);
-          } while (!bmx280.hasValue());
-        
-          Serial.print("Pressure: "); Serial.print(bmx280.getPressure()); Serial.println(" Pa");
-          Serial.print("Pressure: "); Serial.print(bmx280.getPressure() / 100); Serial.println(" mBar");
-          Serial.print("Pressure (64 bit): "); Serial.print(bmx280.getPressure64()); Serial.println(" Pa");
-          Serial.print("Pressure (64 bit): "); Serial.print(bmx280.getPressure64() / 100); Serial.println(" mBar");
-          Serial.print("Temperature: "); Serial.print(bmx280.getTemperature()); Serial.println(" °C");
-        
-          if (bmx280.isBME280())
-          {
-            Serial.print("Humidity: "); Serial.print(bmx280.getHumidity()); Serial.println(" %");
-          }
+        char timestampString[20];
+        sprintf(timestampString,"%04d-%02d-%02d %02d.%02d.%02d", p.getYear(), p.getMonth(), p.getMonthDay(), p.getHours(), p.getMinutes(), p.getSeconds());
+        Serial.println(timestampString);
           
-          HTTPClient http;
-          
-          // Your Domain name with URL path or IP address with path
-          http.begin(serverName);
-    
-          // Specify content-type header
-          http.addHeader("Content-Type", "application/x-www-form-urlencoded");
-          // Data to send with HTTP POST
-          String httpRequestData = "api_key=temperature_logger&timestamp=";
-          httpRequestData += logString;
-          httpRequestData += "&temperature=" + String(bmx280.getTemperature());
-          httpRequestData += "&pressure=" + String(bmx280.getPressure64() / 100);
-          httpRequestData += "&humidity=" + String(bmx280.getHumidity());
-          Serial.println(httpRequestData);
-              
-          // Send HTTP POST request
-          int httpResponseCode = http.POST(httpRequestData);
-         
-          Serial.print("HTTP Response code: ");
-          Serial.println(httpResponseCode);
-            
-          // Free resources
-          http.end();
-        }
-        else
-        {
-            Serial.println("could not start measurement, is a measurement already running?");
-            return;        
-        }
+        pushData(timestampString, temp, pressure, humidity);
       }
     }
-    else {
+    else 
+    {
       Serial.println("WiFi Disconnected");
     }
+  }
+  else
+  {
+      Serial.println("could not start measurement, is a measurement already running?");      
+  }
+}
+
+void loop() 
+{  
+#ifndef DEEP_SLEEP
+  //Send an HTTP POST request every 10 minutes
+  if ((millis() - lastTime) > (MEASURE_PERIOD_SEC * 1000)) // in mSec
+  {
+    doWork();
     lastTime = millis();
   }
+#endif
 }
